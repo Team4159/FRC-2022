c3ebe472dec5c869ca90c5be3502553f6a967669
Cole L <94574122+Mayamelon@users.noreply.github.com>
2022-03-02 15:46:04 -0800

    Camera

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.cameraserver.CameraServer;
 import frc.robot.subsystems.Drivetrain;
 
 /**
@@ -30,7 +31,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-
+    CameraServer.startAutomaticCapture();
   }
 
   /**
