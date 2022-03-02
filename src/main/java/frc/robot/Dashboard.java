900ab623d31e80fd3b2e2366482d4c4ab3f2e20a
L-dunn <73450250+L-dunn@users.noreply.github.com>
2022-03-02 07:41:59 -0800

    testing arm encoder for dashboard

--- src/main/java/frc/robot/Dashboard.java
+++ src/main/java/frc/robot/Dashboard.java
@@ -4,12 +4,12 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.subsystems.Arm;
-import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Feeder;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Shooter;
+//import frc.robot.subsystems.Arm;
+//import frc.robot.subsystems.Climber;
+//import frc.robot.subsystems.Drivetrain;
+//import frc.robot.subsystems.Feeder;
+//import frc.robot.subsystems.Intake;
+//import frc.robot.subsystems.Shooter;
 
 //import com.ctre.phoenix.sensors.Pigeon2;
 //import com.ctre.phoenix.sensors.PigeonIMU;
@@ -20,31 +20,34 @@ public class Dashboard {
     //Call methods here to run them so they do their thing in shuffleboard
     public Dashboard (RobotContainer robotContainer) {
         this.robotContainer = robotContainer;
-        pigeonData();
+        //test();
+        //pigeonData();
         armEncoderData();
-        climberEncoderData();
+        //climberEncoderData();
+
         //lJoystickPort();
         //rJoystickPort();
         //secJoystickPort();
     }
     //Pigeon Gyro, check if it works
-    public void pigeonData() {
-        Shuffleboard.getTab("Electrical")
-            .add("Gyro data", /*hope this works->*/robotContainer.getDriveTrain().getRotation())
-            .withWidget(BuiltInWidgets.kGyro)
+    //public void pigeonData() {
+        /*Shuffleboard.getTab("Electrical")
+            .add("Gyro data", hope this works->robotContainer.getDriveTrain().getRotation())
+            /*.withWidget(BuiltInWidgets.kGyro)
             .withSize(2, 2)
-            .withPosition(4, 0);
-    }
+            .withPosition(4, 0);*/
+    //}
     //Encoders for arm and climber, check if they work
     public void armEncoderData(){
-        Shuffleboard.getTab("Electrical")
+        SmartDashboard.putNumber("Arm Encoder", robotContainer.getArm().getEncoderRaw());
+        /*Shuffleboard.getTab("Electrical")
             .add("Arm Encoder", robotContainer.getArm().getEncoderRaw())
             .withWidget(BuiltInWidgets.kEncoder)
             .withSize(2, 1)
             .withPosition(4, 2)
-            .getEntry();
+            .getEntry();*/
     }
-
+/*
     public void climberEncoderData(){
         Shuffleboard.getTab("Electrical")
             .add("Climber Encoder", robotContainer.getClimber().getEncoderRaw())
@@ -56,7 +59,7 @@ public class Dashboard {
     //Encoders for all motors
     public void motorEncoder1 () {
         //stuff
-    }
+    }*/
 
     /*
     public void intakeEncoderData(){
@@ -68,6 +71,7 @@ public class Dashboard {
 
     //PDP Current Stuff
         //Left Joystick
+/*
     public void lJoystickPort () {
         Shuffleboard.getTab("Electrical")
         //getVoltage returns voltage for whole robot, not a specific port. 
@@ -94,11 +98,11 @@ public class Dashboard {
             .withWidget(BuiltInWidgets.kPowerDistribution);
     }
         //Right front talon
-        public void rTalonFrontPDP () {
-            Shuffleboard.getTab("Electrical")
-                .add("Right Front Talon Current", robotContainer.PDH.getCurrent(1))
-                .withWidget(BuiltInWidgets.kPowerDistribution);
-        }
+    public void rTalonFrontPDP () {
+        Shuffleboard.getTab("Electrical")
+            .add("Right Front Talon Current", robotContainer.PDH.getCurrent(1))
+            .withWidget(BuiltInWidgets.kPowerDistribution);
+    }*/
 
     /*public void PDP_TotalCurrent(){
         electrical
@@ -107,7 +111,14 @@ public class Dashboard {
 
         double leftJoystickCurr = leftJoystickPort.getCurrent();
     }*/
-    public void test (){
-        SmartDashboard.putNumber("Test", 1);
+    /*public String green (){
+        Shuffleboard.getTab("Electrical")
+        .add("Text View", "oo")
+        .withWidget("Text View")
+        .withProperties(Map.of("colorWhenTrue", "green", "colorWhenFalse", "maroon"))
+        .getEntry();
     }
+    public void test (){
+        SmartDashboard.putString("Text View", green());
+    }*/
 }
