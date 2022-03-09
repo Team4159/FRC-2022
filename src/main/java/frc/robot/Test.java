0b83cf118bed9425dcb25911cd56807d17a39b34
AiP24 <aipacifico@s.sfusd.edu>
2022-03-08 21:25:52 -0800

    Created more tests but might break robot so I put it in another branch until it is confirmed working

--- src/main/java/frc/robot/Test.java
+++ src/main/java/frc/robot/Test.java
@@ -5,6 +5,9 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.Direction;
+import frc.robot.commands.AutoCommands.*;
+import frc.robot.commands.CommandGroups.*;
+import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import frc.robot.subsystems.Arm.ArmState;
 
@@ -15,6 +18,7 @@ import frc.robot.subsystems.Arm.ArmState;
 */
 
 public class Test extends SequentialCommandGroup{
+    private MoveDistance moveDistance;
     private Drivetrain drivetrain;
     private Intake intake;
     private Arm arm;
@@ -23,7 +27,6 @@ public class Test extends SequentialCommandGroup{
     private Shooter shooter;
     private Timer timer = new Timer();
     private double outputValue;
-    private double timesBroke; 
     private String fail = "";
     
     public Test(Drivetrain drivetrain, Intake intake, Arm arm, Feeder feeder, Neck neck, Shooter shooter) {
@@ -33,65 +36,114 @@ public class Test extends SequentialCommandGroup{
         this.feeder = feeder;
         this.neck = neck;
         this.shooter = shooter;
+        this.moveDistance = MoveDistance(drivetrain, 3); //TODO: figure out how far that 3 is
         timer.start();
-        timesBroke = 0;
     }
 
     public void Test() {
+        //Ordered based on the order in vscode (alphabetical inside the robot folder)
+        //AutoCommands
         if(timer.get() < 1) {
-            intake.runIntake(Direction.FORWARDS);
-            System.out.println(tolerance(intake.getIntakeSpark().get(), Constants.IntakeAndArmConstants.intakeSpeed, 0.01));
-            if(!tolerance(intake.getIntakeSpark().get(), Constants.IntakeAndArmConstants.intakeSpeed, 0.01)) {
-                timesBroke ++;
-            }
+            moveDistance.execute();
         }
         else if(timer.get() == 1) {
-            if(timesBroke > 0) {
+            if(!moveDistance.isFinished()) { //TODO: isFinished might run early and could give false results but without the robot I can't test it
+                System.out.println("------------------------------");
+                System.out.println("MoveDistance: BROKEN \u2715");
+                fail += "MoveDistance, ";
+            }
+            else {
                 System.out.print("------------------------------");
-                System.out.println("Intake Forwards: " + "\u2715");
-                System.out.print("------------------------------");
+                System.out.print("MoveDistance: OK \u2713");
+            }
+        }
+        
+        //below stuff is kinda broken probably maybe not?
+        //I won't touch it for now but I'll edit it once I get back to subsystems
+        
+        if(timer.get() < 1) {
+            intake.runIntake(Direction.FORWARDS);
+            //spin up motor
+            // System.out.println(tolerance(intake.getIntakeSpark().get(), Constants.IntakeAndArmConstants.intakeSpeed, 0.01));
+            //^ will get annoying after a while, might be good for debugging though
+        }
+        else if(timer.get() == 1) {
+            //give the motor time to spin up and then check if the final speed is within expected levels
+            if(!tolerance(intake.getIntakeSpark().get(), Constants.IntakeAndArmConstants.intakeSpeed, 0.01)) {
+                System.out.println("------------------------------");
+                System.out.println("Intake Forwards: BROKEN \u2715");
+                System.out.println("------------------------------");
                 fail += "Intake Forwards, ";
             }
             else {
                 System.out.print("------------------------------");
-                System.out.print("Intake Forwards: " + "\u2713");
+                System.out.print("Intake Forwards: OK \u2713");
                 System.out.print("------------------------------");
             }
+            //stop intake to prepare for next test
+            intake.stop();
         }
+
         else if(timer.get() < 2) {
             intake.runIntake(Direction.BACKWARDS);
-            System.out.println(tolerance(intake.getIntakeSpark().get(), -Constants.IntakeAndArmConstants.intakeSpeed, 0.01));
-            if(!tolerance(intake.getIntakeSpark().get(), -Constants.IntakeAndArmConstants.intakeSpeed, 0.01)) {
-                timesBroke ++;
-            }
         }
         else if(timer.get() == 2) {
-            if(timesBroke > 0) {
-                System.out.print("------------------------------");
-                System.out.println("Intake Backwards: " + "\u2715");
-                System.out.print("------------------------------");
+            if(!tolerance(intake.getIntakeSpark().get(), -Constants.IntakeAndArmConstants.intakeSpeed, 0.01)) {
+                System.out.println("------------------------------");
+                System.out.println("Intake Backwards: BROKEN \u2715");
+                System.out.println("------------------------------");
                 fail += "Intake Backwards, ";
             }
             else {
-                System.out.print("------------------------------");
-                System.out.print("Intake Backwards: " + "\u2713");
-                System.out.print("------------------------------");
+                System.out.println("------------------------------");
+                System.out.println("Intake Backwards: OK \u2713");
+                System.out.println("------------------------------");
             }
+            //stop intake to prepare for next test
+            intake.stop();
         }
+
         else if(timer.get() < 3) {
             arm.runArm(ArmState.HIGH);
-            if(arm.atSetpoint(Constants.IntakeAndArmConstants.pidHighSetPoint, Constants.IntakeAndArmConstants.tolerance)) {
-                
-            }
         }
         else if(timer.get() == 3) {
-            if(timesBroke > 0) {
-                System.out.println("Intake Backwards: " + "\u2715");
-                fail += "Intake Backwards, ";
+            if(!tolerance(arm.getArmSpark().get(), Constants.IntakeAndArmConstants.pidHighSetPoint, 0.01)) {
+                System.out.println("------------------------------");
+                System.out.println("Arm High/Up: BROKEN \u2715");
+                System.out.println("------------------------------");
+                fail += "Arm High/Up, ";
             }
             else {
-                System.out.print("Intake Backwards: " + "\u2713");
+                System.out.println("------------------------------");
+                System.out.println("Arm High/Up: OK \u2713");
+                System.out.println("------------------------------");
             }
+        }
+        
+        else if(timer.get() < 4) {
+            arm.runArm(ArmState.LOW);
+        }
+        else if (timer.get() == 4) {
+            if(!tolerance(arm.getArmSpark().get(), Constants.IntakeAndArmConstants.pidLowSetPoint, 0.01)) {
+                System.out.println("------------------------------");
+                System.out.println("Arm Low/Down: BROKEN \u2715");
+                System.out.println("------------------------------");
+                fail += "Arm Low/Down, ";
+            }
+            else {
+                System.out.println("------------------------------");
+                System.out.println("Arm Low/Down: OK \u2713");
+                System.out.println("------------------------------");
+            }
+        }
+
+        else if(timer.get() < 5) {
+
+        }
+
+        //display all broken subsystems
+        else if (timer.get() == 30) {//TODO: 30 is temporary
+            System.out.println("Broken Commands/Subsystems: " + fail);
         }
     }
 
