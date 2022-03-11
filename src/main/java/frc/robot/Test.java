107bffd9578376d04e5cd007a0d543b2cf96608b
AiP24 <aipacifico@s.sfusd.edu>
2022-03-10 21:55:29 -0800

    TurnDegrees/Climb/Arm/Feeder(?) tests

--- src/main/java/frc/robot/Test.java
+++ src/main/java/frc/robot/Test.java
@@ -15,28 +15,54 @@ import frc.robot.subsystems.Arm.ArmState;
 /*
     Runs a ~ 30 second test before we switch the battery after every match to ensure all subsystems + commands are running correctly
     Test once you enable the test mode in Driverstation
+
+    Commands not tested: Drive (needs joystick input)
 */
 
 public class Test extends SequentialCommandGroup{
-    private MoveDistance moveDistance;
-    private Drivetrain drivetrain;
-    private Intake intake;
+    //Subsystems
     private Arm arm;
+    private Climber climber;
+    private Drivetrain drivetrain;
     private Feeder feeder;
+    private Intake intake;
     private Neck neck;
     private Shooter shooter;
+    //AutoCommands
+    private MoveDistance moveDistance;
+    private TurnDegrees turnDegrees;
+    //CommandGroups
+    private Climb climbRaise;
+    private Climb climbLower;
+    private MoveArm moveArmLow;
+    private MoveArm moveArmHigh;
+    private RunFeeder runFeederForwards;
+    private RunFeeder runFeederBackwards;
+    //Commands
+    //Variables
     private Timer timer = new Timer();
     private double outputValue;
     private String fail = "";
     
-    public Test(Drivetrain drivetrain, Intake intake, Arm arm, Feeder feeder, Neck neck, Shooter shooter) {
-        this.drivetrain = drivetrain;
+    public Test(Arm arm, Climber climber, Drivetrain drivetrain, Feeder feeder, Intake intake, Neck neck, Shooter shooter) {
+        //subsystems
         this.arm = arm;
-        this.intake = intake;
+        this.climber = climber;
+        this.drivetrain = drivetrain;
         this.feeder = feeder;
+        this.intake = intake;
         this.neck = neck;
         this.shooter = shooter;
+        //AutoCommands
         this.moveDistance = MoveDistance(drivetrain, 3); //TODO: figure out how far that 3 is
+        this.turnDegrees = TurnDegrees(drivetrain, 360);
+        //CommandGroups
+        this.climbRaise = Climb(climber, Climber.ClimberState.RAISE);
+        this.climbLower = Climb(climber, Climber.ClimberState.LOWER);
+        this.moveArmHigh = MoveArm(arm, Arm.ArmState.HIGH);
+        this.moveArmLow = MoveArm(arm, Arm.ArmState.LOW);
+        this.runFeederForwards = RunFeeder(feeder, Constants.Direction.FORWARDS);
+        this.runFeederBackwards = RunFeeder(feeder, Constants.Direction.BACKWARDS);
         timer.start();
     }
 
@@ -57,6 +83,103 @@ public class Test extends SequentialCommandGroup{
                 System.out.print("MoveDistance: OK \u2713");
             }
         }
+        
+        else if(timer.get() < 2) {
+            turnDegrees.execute();
+        }
+        else if(timer.get() == 2) {
+            if(!turnDegrees.isFinished()) {
+                System.out.println("------------------------------");
+                System.out.println("TurnDegrees: BROKEN \u2715");
+                fail += "TurnDegrees, ";
+            }
+            else {
+                System.out.print("------------------------------");
+                System.out.print("TurnDegrees: OK \u2713");
+            }
+        }
+
+        //CommandGroups
+        else if(timer.get() < 3) {
+            climbRaise.execute();
+        }
+        else if(timer.get() == 3) {
+            if(!tolerance(climber.getEncoderRaw(), climber.getHighSetPoint(), 0.01)) {
+                System.out.println("------------------------------");
+                System.out.println("Climb (Raise): BROKEN \u2715");
+                fail += "Climb (Raise), ";
+            }
+            else {
+                System.out.print("------------------------------");
+                System.out.print("Climb (Raise): OK \u2713");
+            }
+        }
+
+        else if(timer.get() < 4) {
+            climbLower.execute();
+        }
+        else if(timer.get() == 4) {
+            if(!tolerance(climber.getEncoderRaw(), climber.getLowSetPoint(), 0.01)) {
+                System.out.println("------------------------------");
+                System.out.println("Climb (Lower): BROKEN \u2715");
+                fail += "Climb (Lower), ";
+            }
+            else {
+                System.out.print("------------------------------");
+                System.out.print("Climb (Lower): OK \u2713");
+            }
+        }
+        
+        else if(timer.get() < 5) {
+            moveArmHigh.execute();
+        }
+        else if(timer.get() == 5) {
+            if(!arm.atSetpoint(Constants.IntakeAndArmConstants.pidHighSetPoint, 0.01)) {
+                System.out.println("------------------------------");
+                System.out.println("Arm (High): BROKEN \u2715");
+                fail += "Arm (High), ";
+            }
+            else {
+                System.out.print("------------------------------");
+                System.out.print("Arm (High): OK \u2713");
+            }
+        }
+
+        else if(timer.get() < 6) {
+            moveArmLow.execute();
+        }
+        else if(timer.get() == 6) {
+            if(!arm.atSetpoint(Constants.IntakeAndArmConstants.pidLowSetPoint, 0.01)) {
+                System.out.println("------------------------------");
+                System.out.println("Arm (Low): BROKEN \u2715");
+                fail += "Arm (Low), ";
+            }
+            else {
+                System.out.print("------------------------------");
+                System.out.print("Arm (Low): OK \u2713");
+            }
+        }
+
+        //TODO: Feeder doesn't actually have anything to check
+        //Please somebody find something to check
+        else if(timer.get() < 7) {
+            runFeederForwards.execute();
+        }
+        else if(timer.get() < 8) {
+            runFeederBackwards.execute();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
         
         //below stuff is kinda broken probably maybe not?
         //I won't touch it for now but I'll edit it once I get back to subsystems
